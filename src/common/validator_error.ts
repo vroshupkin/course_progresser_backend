4fddb71d62e3a1ad38a6ef662e8c5ea504ae2d14
vroshupkin <vroshupkin1@gmail.com>
2023-04-24 16:27:43 +0300

    Черновая версия валидаторов

---
+++ src/common/validator_error.ts
@@ -0,0 +1,43 @@
+import { N_aryOperator } from './common.types';
+
+
+export interface ValidatorError{
+    validationType: string,
+    message: string,
+}
+
+const ErrorValidatorFactory = <Args extends any[]>
+  (handler: N_aryOperator<Args, boolean>) => 
+    (...args: Args) =>
+      (message: string): ValidatorError | null =>
+      {
+        if(!handler(...args))
+        {
+          return {
+            validationType: handler.name,
+            message: message
+          };
+        }
+        
+        return null;
+      };
+
+const existInObj = (obj: {[s: string | number]: any}, key: string | number) =>  obj?.[key] !== undefined;
+const greater_than = (a: number, b: number) => a > b;
+const less_than = (a: number, b: number) => a < b;
+const IsNotUndefiend = (a: any) => a != undefined;
+const IsNotNull = ((a: any) => a != null);
+
+export const Validators = 
+{
+  IsNotNull: ErrorValidatorFactory(IsNotNull),
+  IsNotUndefiend: ErrorValidatorFactory(IsNotUndefiend),
+};
+
+
+export const LessThanValidator  = ErrorValidatorFactory(less_than);
+export const GreaterThanValidator  = ErrorValidatorFactory(greater_than);
+export const ExistInObjValidator  = ErrorValidatorFactory(existInObj);
+export const NotUndefiendValidator  = ErrorValidatorFactory(IsNotUndefiend);
+
+
