0e1095ca2cdab6553506ed139c2c7ec56c6816d5
vroshupkin <vroshupkin1@gmail.com>
2023-05-27 12:33:07 +0300

    Преименование notify schema

---
+++ src/notify/notify.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from 'src/users/users.schema';
+
+@Schema()
+export class ChatId
+{
+    @Prop({ type: mongoose.Schema.Types.ObjectId, unique: true, ref: 'users' })
+      user: mongoose.Types.ObjectId;
+      
+    @Prop()
+      chat_id: string; 
+}
+
+export type ChatIdDocument = HydratedDocument<ChatId>;
+export const ChatIdSchema = SchemaFactory.createForClass(ChatId);
+
